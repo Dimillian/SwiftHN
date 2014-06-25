87cb14c659660908136e3be888fae5ca231cbaeb
Thomas Ricouard <ricouard77@gmail.com>
2014-06-25 21:26:27 +0200

    Automatically add detail VC in navController (iPad)

--- SwiftHN/ViewControllers/SplitViewController.swift
+++ SwiftHN/ViewControllers/SplitViewController.swift
@@ -14,10 +14,21 @@ class SplitViewController: UISplitViewController, UISplitViewControllerDelegate 
         super.viewDidLoad()
         
         self.delegate = self
-        self.preferredDisplayMode = UISplitViewControllerDisplayMode.AllVisible
+        self.preferredDisplayMode = UISplitViewControllerDisplayMode.Automatic
+        self.minimumPrimaryColumnWidth = 320.0
     }
     
     func splitViewController(splitViewController: UISplitViewController!, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
         return true;
+    }
+    
+    override func showDetailViewController(vc: UIViewController!, sender: AnyObject!) {
+        if (vc.parentViewController is UINavigationController) {
+            super.showDetailViewController(vc, sender: sender)
+        }
+        else {
+            var nav = UINavigationController(rootViewController: vc)
+            super.showDetailViewController(nav, sender: sender)
+        }
     }
 }
