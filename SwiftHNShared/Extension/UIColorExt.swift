eca4a8d565fa2c8c7da3c75abcf885e4b9c4fd6f
Thomas Ricouard <ricouard77@gmail.com>
2014-06-25 21:25:52 +0200

    Add new custom color

--- SwiftHNShared/Extension/UIColorExt.swift
+++ SwiftHNShared/Extension/UIColorExt.swift
@@ -10,17 +10,25 @@ import UIKit
 
 extension UIColor {
     
-    class func RGBColor(red: Float, green: Float, blue: Float) -> UIColor {
+    class func RGBColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
     }
     
     class func HNColor() -> UIColor {
         return UIColor.RGBColor(252, green: 102, blue: 33)
     }
+    
+    class func ReadingListColor() -> UIColor {
+        return UIColor.RGBColor(74, green: 144, blue: 226)
+    }
 }
 
 class UIColorEXT {
     class func HNColor() -> UIColor {
         return UIColor.HNColor()
+    }
+    
+    class func ReadtListColor() -> UIColor {
+        return UIColor.ReadingListColor()
     }
 }
