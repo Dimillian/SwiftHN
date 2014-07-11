6151dd692ae4ec7a38a307ef8529689640dd2b3e
Thomas Ricouard <ricouard77@gmail.com>
2014-07-11 22:16:26 +0200

    Update to latest LibHn

--- SwiftHNTests/SwiftHNTests.swift
+++ SwiftHNTests/SwiftHNTests.swift
@@ -12,6 +12,7 @@ class SwiftHNTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
