427c80614fb7c0a06a0e4ad5accff11874b7cf98
Thomas Ricouard <ricouard77@gmail.com>
2015-03-10 19:45:08 +0100

    Watch app

---
+++ Hacker News WatchKit Extension/CommentsInterfaceController.swift
@@ -0,0 +1,46 @@
+//
+//  CommentsInterfaceController.swift
+//  SwiftHN
+//
+//  Created by Thomas Ricouard on 10/03/15.
+//  Copyright (c) 2015 Thomas Ricouard. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import HackerSwifter
+
+class HNCommentRow: NSObject {
+    @IBOutlet var commentLabel: WKInterfaceLabel!
+    @IBOutlet var usernameLabel: WKInterfaceLabel!
+}
+
+class CommentsInterfaceController: WKInterfaceController {
+    var post: Post!
+    var datasource: [Comment]!
+    
+    @IBOutlet var tableView: WKInterfaceTable!
+    override func awakeWithContext(context: AnyObject?) {
+        if let realPost = context as? Post {
+            self.post = realPost
+            
+            Comment.fetch(forPost: self.post, completion: {(comments: [Comment]!, error: Fetcher.ResponseError!, local: Bool) in
+                self.datasource = comments
+                self.setupTable()
+            })
+        }
+    }
+    
+    func setupTable() {
+        self.tableView.setNumberOfRows(self.datasource.count, withRowType: "HNCommentRow")
+        var rowCount = 0
+        for item in self.datasource {
+            var row = self.tableView.rowControllerAtIndex(rowCount) as HNCommentRow
+            if let realtext = item.text {
+                row.commentLabel.setText(realtext)
+                row.usernameLabel.setText(item.username)
+            }
+            rowCount++
+        }
+    }
+}
---
+++ Hacker News WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,64 @@
+//
+//  InterfaceController.swift
+//  Hacker News WatchKit Extension
+//
+//  Created by Thomas Ricouard on 10/03/15.
+//  Copyright (c) 2015 Thomas Ricouard. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import HackerSwifter
+
+class HNNewsRow: NSObject {
+    @IBOutlet var voteLabel: WKInterfaceLabel!
+    @IBOutlet var titleLabel: WKInterfaceLabel!
+    @IBOutlet var usernameLabel: WKInterfaceLabel!
+    @IBOutlet var domainLabel: WKInterfaceLabel!
+}
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var tableView: WKInterfaceTable!
+    var datasource: [Post]!
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        Post.fetch(Post.PostFilter.Top, completion: {(posts: [Post]!, error: Fetcher.ResponseError!, local: Bool) in
+            self.datasource = posts
+            self.setupTable();
+        })
+
+    }
+    
+    func setupTable() {
+        self.tableView.setNumberOfRows(self.datasource.count, withRowType: "HNNewsRow")
+        var rowCount = 0
+        for item in self.datasource {
+            var row = self.tableView.rowControllerAtIndex(rowCount) as HNNewsRow
+            row.voteLabel.setText(String(item.points))
+            row.titleLabel.setText(item.title)
+            row.usernameLabel.setText(String(item.commentsCount) + " Comments")
+            row.domainLabel.setText(item.domain)
+            rowCount++
+        }
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        var post = self.datasource[rowIndex]
+        presentControllerWithName("commentsController", context: post)
+    }
+    
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
